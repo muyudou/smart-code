65c8e3bfaa221f4926f4fdc262b8a1460da7a5a1
caoxiaoyong <caoxyemail@163.com>
2018-05-26 22:14:21 +0800

    update remove duplicates from sorted array by use o(n)

--- cxy/src/main/java/array/RemoveDuplicatesSortedArray.java
+++ cxy/src/main/java/array/RemoveDuplicatesSortedArray.java
@@ -2,9 +2,12 @@ package array;
 
 /**
  * 删除有序数组中的重复元素，要求时间复杂度为O(1)
- * 思路：由于数组是有序的，因此相同的元素是相邻的，因此可以使用两次遍历：
+ * 思路1：由于数组是有序的，因此相同的元素是相邻的，因此可以使用两次遍历：
  * 1） 标记那些与前一个位置相同的元素，把它们设为一个特殊值
  * 2) 第二次遍历，从index 1处开始，把未标记的元素拷贝到一起
+ *
+ * 思路2: 压根不需要第一遍的标记，使用一个index来作为有效元素的存储，当遇到相同元素时忽略，
+ * 遇到不同元素时，把它放到index处，index后移
  *
  * @author yuhongye
  * @date   2018-05-26
@@ -43,5 +46,20 @@ public class RemoveDuplicatesSortedArray {
             }
         }
         return index + 1;
+    }
+
+    public int removeDuplicate2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        // index下一个有效位置
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[index - 1]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
     }
 }
